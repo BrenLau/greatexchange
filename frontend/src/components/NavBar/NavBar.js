f4733830105df44ca8050cf31a1ae2ae13a72279
BrenLau <blau4000@gmail.com>
2023-02-02 19:17:27 -0500

    chest button works

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -4,16 +4,34 @@ import './navbar.css'
 import UserButtons from './UserButtons';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import box from './treasure.png'
+import open from './open.png'
 
 const NavBar = () => {
   const user = useSelector(state => state.session.user)
   const [loginbutts, setloginbutts] = useState(false)
+  const [openBag, setOpenBag] = useState(false)
+
   return (
     <nav className='navbar-container'>
+      {openBag ? <ul id='dropdown'>
+        <li className='navbarli'>
+          market
+        </li>
+        <li className='navbarli'>
+          offers
+        </li>
+      </ul> : null}
       <li className='navbarli'>
         <NavLink className='navlinknav' to='/' exact={true} activeclassname='active'>
           Home
         </NavLink>
+      </li>
+      <li className='navbarli bagicon' onClick={(e) => {
+        e.preventDefault()
+        setOpenBag(!openBag)
+      }} id='bagicon'>
+        <img className='boximage' src={openBag ? open : box} ></img>
       </li>
       <li className='navbarli'>
         <NavLink className='navlinknav' to='/users' exact={true} activeclassname='active'>
