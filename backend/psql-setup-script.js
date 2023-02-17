87940b4abd70174ceac08d7e89ea08a790e910c9
BrenLau <blau4000@gmail.com>
2023-02-16 19:57:24 -0500

    models

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,4 +1,4 @@
-const { sequelize } = require('./db/models');
+const { sequelize } = require('./models');
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
     if (!data.includes(process.env.SCHEMA)) {
