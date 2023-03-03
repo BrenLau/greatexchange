4ac5dae3e1dd4238474af12f535e9bbb81d200b2
BrenLau <blau4000@gmail.com>
2023-03-03 15:03:05 -0500

    nav on logout

--- frontend/src/components/auth/LogoutButton.js
+++ frontend/src/components/auth/LogoutButton.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
 import "./logout.css"
+import { useNavigate } from 'react-router';
+
 
 const LogoutButton = () => {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
+    navigate('/', { replace: true })
   };
 
   return <div className='logoutdiv' onClick={onLogout}>Logout</div>;
