48902b7c59cd95a987bf94c8bb594ec98898ea2a
BrenLau <blau4000@gmail.com>
2023-03-24 15:30:42 -0400

    seeking username now brings to user page

--- frontend/src/components/seeking/Seekings.js
+++ frontend/src/components/seeking/Seekings.js
@@ -4,8 +4,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getSeekingsThunk } from '../../store/seeking'
 import DeleteSeeking from "./DeleteSeeking";
 import SeekingModal from "./SeekingModal";
+import { useNavigate } from "react-router-dom";
 
 const Seeking = () => {
+    const navigate = useNavigate()
     const dispatch = useDispatch()
     const seekings = useSelector(state => state.seekings)
     const [seekingModal, setSeekingModal] = useState(false)
@@ -32,7 +34,11 @@ const Seeking = () => {
                             <div className="outsidedivseeking">Item Name: <div>{seeking.name}</div></div>
                             <div className="outsidedivseeking">Summary: <div>{seeking.summary}</div></div>
                             {seeking.image ? <img className='itemimage' src={seeking?.image}></img> : <img className='itemimage' src='https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg'></img>}
-                            <div className="outsidedivseeking">Seeker: <div>{seeking.User.username}</div></div>
+                            <div onClick={(e) => {
+                                e.stopPropagation()
+                                navigate(`/user/${seeking?.User?.id}`, { replace: true })
+
+                            }} className="outsidedivseeking">Seeker: <div>{seeking.User.username}</div></div>
                         </div>)
                 })}
             </div>
