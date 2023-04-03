82ae5fd01ca2ed0300c4026774d5eb8d031e8234
BrenLau <blau4000@gmail.com>
2023-04-03 19:42:26 -0400

    no console logs

--- backend/routes/api/message.js
+++ backend/routes/api/message.js
@@ -16,7 +16,6 @@ router.post('/',
     asyncHandler(async (req, res) => {
         const { user } = req
         const { content, receiverId, senderId } = req.body
-        console.log(receiverId)
         if (!user || user.id != senderId) {
             res.json({ message: 'Must be logged in' })
             return null
@@ -24,7 +23,6 @@ router.post('/',
 
         const privatemessage = await PrivateMessage.create({ content, senderId: user.id, receiverId })
         const privatemessage1 = await PrivateMessage.findOne({ where: { id: privatemessage.id }, include: [{ model: User, as: 'sender' }, { model: User, as: 'receiver' }] })
-        console.log(privatemessage1)
         res.json(privatemessage1)
 
     }))
@@ -34,7 +32,6 @@ router.get('/user/:userId',
     asyncHandler(async (req, res) => {
         const { userId } = req.params
         const messages = await PrivateMessage.findAll({ where: { [Op.or]: [{ senderId: userId }, { receiverId: userId }] }, include: [{ model: User, as: 'sender' }, { model: User, as: 'receiver' }] })
-        console.log(messages)
         res.json({ messages })
     }))
 
